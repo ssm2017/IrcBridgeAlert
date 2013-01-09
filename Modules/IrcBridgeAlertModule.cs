4680059ebd61e119689769824f0b2b4fe29139df
ssm2017 <ssm2017.binder@gmail.com>
2013-01-09 18:50:17 +0100

    cleaned spaces and tabs

--- Modules/IrcBridgeAlertModule.cs
+++ Modules/IrcBridgeAlertModule.cs
@@ -17,38 +17,37 @@ using Mono.Addins;
 
 namespace ModIrcBridgeAlertModule
 {
-    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule")]
-    public class AvatarServicesModule : ISharedRegionModule
-    {
-        #region Fields
-        private static readonly ILog m_log = LogManager.GetLogger (MethodBase.GetCurrentMethod ().DeclaringType);
+	[Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule")]
+	public class AvatarServicesModule : ISharedRegionModule
+	{
+		#region Fields
+		private static readonly ILog m_log = LogManager.GetLogger (MethodBase.GetCurrentMethod ().DeclaringType);
 		private IDialogModule m_dialogModule;
 
-        public string m_name = "IrcBridgeAlertModule";
+		public string m_name = "IrcBridgeAlertModule";
 		public string alert_message = "Warning IrcBridge active";
-        private bool m_enabled = false;
-        private Dictionary<string, Scene> m_scenel = new Dictionary<string, Scene> ();
+		private bool m_enabled = false;
+		private Dictionary<string, Scene> m_scenel = new Dictionary<string, Scene> ();
 
-        public IConfigSource m_config;
-        public Scene m_world;
-        public RegionInfo m_regionInfo;
-        public Dictionary<string, Scene> mScene = new Dictionary<string, Scene> ();
-        #endregion
+		public IConfigSource m_config;
+		public Scene m_world;
+		public RegionInfo m_regionInfo;
+		public Dictionary<string, Scene> mScene = new Dictionary<string, Scene> ();
+		#endregion
 
-        #region ISharedRegionModule implementation
-        public void PostInitialise ()
-        {
-        }
+		#region ISharedRegionModule implementation
+		public void PostInitialise () {}
+		#endregion
 
-        #endregion
-
-        #region IRegionModuleBase implementation
-        public void Initialise (IConfigSource source)
-        {
+		#region IRegionModuleBase implementation
+		public void Initialise (IConfigSource source)
+		{
 			m_config = source;
 			IConfig cnf = source.Configs["IRC"];
-			if (cnf != null) {
-				if (cnf.GetBoolean("enabled", false)) {
+			if (cnf != null)
+			{
+				if (cnf.GetBoolean("enabled", false))
+				{
 					m_enabled = true;
 					alert_message = cnf.GetString("alert_msg_pre", "This region is linked to irc.")+ "\n"
 						+ " \nServer "+ cnf.GetString("server", "")
@@ -57,60 +56,55 @@ namespace ModIrcBridgeAlertModule
 						+ "\n"+ cnf.GetString("alert_msg_post", "\nEverything you say in public chat can be listened.\nSee http://opensimulator.org/wiki/IRCBridgeModule for more informations.");
 				}
 			}
-        }
+		}
 
+		public void Close () {}
 
-        public void Close ()
-        {
-
-        }
-
-
-        public void AddRegion (Scene scene)
-        {
-			if (m_enabled) {
+		public void AddRegion (Scene scene)
+		{
+			if (m_enabled)
+			{
 				m_log.InfoFormat ("[IrcBridgeAlertModule]: Region {0} is using irc", scene.RegionInfo.RegionName);
 				scene.EventManager.OnMakeRootAgent += OnMakeRootAgent;
 			}
-        }
+		}
 
-        public void RemoveRegion (Scene scene)
-        {
+		public void RemoveRegion (Scene scene)
+		{
 			scene.EventManager.OnMakeRootAgent -= OnMakeRootAgent;
-            if (m_scenel.ContainsKey (scene.RegionInfo.RegionName)) {
-                lock (m_scenel) {
-                    m_scenel.Remove (scene.RegionInfo.RegionName);
-                }
-            }
-        }
+			if (m_scenel.ContainsKey (scene.RegionInfo.RegionName))
+			{
+				lock (m_scenel)
+				{
+					m_scenel.Remove (scene.RegionInfo.RegionName);
+				}
+			}
+		}
 
-
-        public void RegionLoaded (Scene scene)
-        {
-            if (m_enabled == false)
-                return;
+		public void RegionLoaded (Scene scene)
+		{
+			if (m_enabled == false)
+				return;
 			m_dialogModule = scene.RequestModuleInterface<IDialogModule>();
-        }
+		}
 
+		public string Name {
+			get { return m_name; }
+		}
 
-        public string Name {
-            get { return m_name; }
-        }
+		public Type ReplaceableInterface
+		{
+			get { return null; }
+		}
 
+		#endregion
 
-        public Type ReplaceableInterface {
-            get { return null; }
-        }
-
-        #endregion
-
-        #region IrcBridgeAlertModule
-
-		void OnMakeRootAgent (ScenePresence presence) {
+		#region IrcBridgeAlertModule
+		void OnMakeRootAgent (ScenePresence presence)
+		{
 			IClientAPI client = presence.ControllingClient;
 			m_dialogModule.SendAlertToUser(client, alert_message, true);
 		}
-
-        #endregion
-    }
+		#endregion
+	}
 }
